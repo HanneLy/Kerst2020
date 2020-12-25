7440a8ac2f49fba7450c2296a1a1d7be188f5f7a
HanneLy <hanne.lyssens@gmail.com>
2020-12-26 00:44:38 +0100

    Update Interaction2.js

--- Interaction2.js
+++ Interaction2.js
@@ -6,7 +6,7 @@ function main() {
 var WWinput = document.getElementById("IngevoerdWW")
 
 document.getElementById("verzendknop1").onclick = function WWsubmitten () {
-  if (WWinput.value==="Hoi") {if(confirm("Ben je zeker dat je wil verdergaan?")){document.getElementById("video1").style.display="block"};
+  if (WWinput.value==="redesaikeektiko") {if(confirm("Goed gepuzzeld! Benieuwd wat hierachter zit? Klik op OK")){document.getElementById("video1").style.display="block"};
   }
   else if (WWinput.value==="Verdorie") {if(confirm("Hey, zeg! Niet vloeken, he!?")){document.getElementById("video1").style.display="block"}
   }

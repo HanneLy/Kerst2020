ffd59afe22db843dcaad255c62bcdfc09b7ae038
HanneLy <hanne.lyssens@gmail.com>
2021-01-01 21:05:14 +0100

    Update Interaction3.js

--- Interaction3.js
+++ Interaction3.js
@@ -6,7 +6,7 @@ function main() {
 var WWinput = document.getElementById("IngevoerdWW")
 
 document.getElementById("verzendknop1").onclick = function WWsubmitten () {
-  if (WWinput.value==="681134517") {if(confirm("Hopelijk verschijnt er deze keer wel een verrassing! Klik op Go!")){document.getElementById("video1").style.display="block"};
+  if (WWinput.value==="681134517") {if(confirm("Hopelijk verschijnt er deze keer wel een verrassing! Klik op Ok!")){document.getElementById("video1").style.display="block"};
   }
   else if (WWinput.value==="Verdorie") {if(confirm("Hey, zeg! Niet vloeken, he!?")){document.getElementById("video1").style.display="block"}
   }
